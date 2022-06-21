72498d7d0e07249e6e9b26529a448abac533fdcb
emonShimoul <emonshimoul@gmail.com>
2022-06-21 11:01:27 +0600

    install and configure nodemon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from Node !!');
+    res.send('I am learning Node !!');
 });
 
 app.listen(port, () => {
